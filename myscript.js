1e42cef33eb478e0c947feb5ad8be13ac9919b90
JackSparring420 <emanuele.arconte94@gmail.com>
2021-11-02 18:00:59 +0100

    exercise complited

--- myscript.js
+++ myscript.js
@@ -1,7 +1,6 @@
 //1. Chiuedo al'utente che difficolta vuole
 //  1.1 creo un prompt in cui gli chiedo il grodo di difficoltà
 //2. Creo le condizioni per la quale varia il numero di quadrati in base alla difficoltà
-//2.1 il numero di quadrati totale deve variare in relazione al n di righe
 //3. Creo un ciclo dove genero i quadrati
 //4. Aggiungo un addEventListener ("click"... dentro il ciclo
 //  4.1  aggiungo la classe "click-true" per cambiargli lo sfondo
@@ -14,35 +13,35 @@ const livello = prompt("Scegli: facile, medio, difficile")
 
 let difficolta;
 if(livello == "facile") {
-    difficolta = 7
+    difficolta = 100
+    dimensione = "facile"
 } else if( livello == "medio"){
-    difficolta = 9
+    difficolta = 81
+    dimensione = "medio"
 } else if (livello== "difficile"){
-    difficolta = 10
+    difficolta = 49;
+    dimensione ="difficile";
 }
 
-for(let i = 0; i < difficolta; i++){
-        let newElement = elementGen("div", "row");
-        gridCont.appendChild(newElement)
+
+for(let i = 0; i < difficolta; i++) {
+    let newElement = elementGen("div", "square", dimensione);
+    
+    newElement.addEventListener("click",
+    function(){
+        this.classList.add("click-true")
+        newElement.innerHTML += `${i+1}`
+        }
+    )
+
+    gridCont.appendChild(newElement)
 }
-
-// for(let i = 0; i < difficolta; i++) {
-//     let newElement = elementGen("div", "square");
-//     newElement.innerHTML += `${i+1}`
-
-//     newElement.addEventListener("click",
-//         function(){
-//             this.classList.add("click-true")
-//         }
-//     )
-
-//     gridCont.appendChild(newElement)
-// }
 
 // funzioni utili
 
-function elementGen(elementGen, classPlus) {
+function elementGen(elementGen, classPlus, classPlusB) {
     let node= document.createElement(elementGen);
     node.classList.add(classPlus)
+    node.classList.add(classPlusB)
     return node
 }
